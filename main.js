49b5fdc1c3e08a335dc6f64fbb04c551c9afdc61
Alba De biase <debiasealba@gmail.com>
2020-10-15 02:05:00 +0200

    ha vinto /ha perso

--- main.js
+++ main.js
@@ -3,52 +3,46 @@
 // Comunicare quali dei due utenti (tra giocatore 1 e giocatore 2) ha vinto.
 
 
-var giocata1 = [1, 2, 3, 4, 5 ,6];
+var giocate = [1, 2, 3, 4, 5 ,6];
 
-Math.floor(Math.random()*7);
+var giocata1 = Math.floor(Math.random()*6 +1);
 document.getElementById("giocata1").innerHTML =
-Math.floor(Math.random() * 7);
+giocata1;
 
-Math.floor(Math.random()*7);
+var giocata2 = Math.floor(Math.random()*6 +1);
 document.getElementById("giocata2").innerHTML =
-Math.floor(Math.random() * 7);
+giocata2;
 
 // se giocata 1 è >6 ha vinto
-//
 // se giocata 2 è >6 ha vinto
-var haVinto = false;
-for(var i = 0; i < giocata1.length; i++) {
-    console.log(giocata1[i]);
 
-if (giocata2 == giocata1[i]) {
-// console.log("giocata2");
-haVinto = true;
-
-    }
+if ( giocata1 > giocata2 ) {
+document.getElementById('giocata1').innerHTML+= "ha vinto"
+document.getElementById('giocata2').innerHTML+= "ha perso"
 }
 
-if (haVinto == true) {
-document.getElementById("giocata1").innerHTML = "ha vinto";
-
-} else {
-    document.getElementById("giocata2").innerHTML = "ha vinto";
+else if ( giocata1 < giocata2 ) {
+document.getElementById('giocata2').innerHTML+= "ha vinto"
+document.getElementById('giocata1').innerHTML+= "ha perso"
 }
 
 
-var haVinto = false;
-for(var i = 0; i < giocata2.length; i++) {
-    console.log(giocata2[i]);
 
-if (giocata1 == giocata2[i]) {
-// console.log("giocata2");
-haVinto = true;
 
-    }
-}
-
-if (haVinto == true) {
-document.getElementById("giocata1").innerHTML = "ha vinto";
-
-} else {
-    document.getElementById("giocata2").innerHTML = "ha perso";
-}
+// var haVinto = false;
+// for(var i = 0; i < giocata1.length; i++) {
+//     console.log(giocata1[i]);
+//
+// if (giocata2 == giocata1[i]) {
+// // console.log("giocata2");
+// haVinto = true;
+//
+//     }
+// }
+//
+// if (haVinto == true) {
+// document.getElementById("giocata1").innerHTML = "ha vinto";
+//
+// } else {
+//     document.getElementById("giocata2").innerHTML = "ha vinto";
+// }
